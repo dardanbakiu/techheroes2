46d640b6726eac6bedc0e6ce6f1a3319ee29f2d8
Dardan Bakiu <dardan.bakiu@gmail.com>
2020-12-16 21:42:33 +0100

    verify

---
+++ static/scripts/verifiko.js
@@ -0,0 +1,16 @@
+
+window.addEventListener('load', (event) => {
+    let seconds = document.getElementById('seconds').innerHTML
+    seconds = parseInt(seconds)
+
+
+    setInterval(() => {
+        seconds = seconds - 1
+        document.getElementById('seconds').innerHTML = seconds
+        if (seconds <= 0) {
+            window.location.href = "http://localhost:3000/";
+        }
+    }, 1000)
+
+
+});
